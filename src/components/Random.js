57f5f920680b6a406d457c19d9a786e945bc1c6f
Gaurav Singh <sumitgaurav6295@gmail.com>
2024-02-19 20:58:25 +0530

    completed random meme

--- src/components/Random.js
+++ src/components/Random.js
@@ -1,43 +1,36 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+import useGif from "../hooks/useGif";
 import Spinner from "./Spinner";
 
 const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
+
 const Random = () => {
-  const [gif, setGif] = useState("");
+  // const [gif, setGif] = useState("");
+  // const [loading, setLoading] = useState(false);
 
-  const [loading, setLoading] = useState(false);
+  // async function fetchData() {
+  //   setLoading(true)
+  //   const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
 
-  async function fetchData() {
-    setLoading(true);
-    const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
-    const { data } = await axios.get(url);
-    const imageSource = data.data.images.downsized_large.url;
-    setGif(imageSource);
-    setLoading(false);
-  }
+  //   const { data } = await axios.get(url);
+  //   const imageSource = data.data.images.downsized_large.url;
+  //   setGif(imageSource);
+  //   console.log(imageSource);
+  //   setLoading(false);
+  // }
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+  // useEffect(() => {
+  //   fetchData();
+  // }, []);
 
-  function clickHandler() {
-    fetchData();
-  }
+  const { gif, loading, fetchData } = useGif();
+
   return (
-    <div
-      className="w-1/2  bg-green-500 rounded-lg border border-black
-     flex flex-col items-center gap-y-5 mt-[15px]"
-    >
-      <h1 className="mt-[15px] text-2xl underline uppercase font-bold">
-        A Random GIF
-      </h1>
-
+    <div className="w-1/2 h-[450px] bg-green-500 mx-auto rounded-lg border border-black flex flex-col items-center gap-y-5 mt-[15px]">
+      <h1 className="text-3xl uppercase underline font-bold">A Random Gif</h1>
       {loading ? <Spinner /> : <img src={gif} width="450" />}
-
       <button
-        className="w-10/12 bg-yellow-500 text-lg py-2 rounded-lg mb-[20px]"
-        onClick={clickHandler}
+        onClick={() => fetchData()}
+        className="w-10/12 bg-white text-xl py-2 rounded-lg font-bold"
       >
         Generate
       </button>
--- src/components/Tag.js
+++ src/components/Tag.js
@@ -1,26 +1,28 @@
 import React, { useState } from "react";
-// import useGif from "../hooks/useGif";
+import useGif from "../hooks/useGif";
 import Spinner from "./Spinner";
 
 const Tag = () => {
   const [tag, setTag] = useState("Dog");
-  const [gif, setGif] = useState("");
-  const [loading, setLoading] = useState(false);
+  // const [gif, setGif] = useState("");
+  // const [loading, setLoading] = useState(false);
 
-  async function fetchData() {
-    setLoading(true);
-    const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&tag=${tag}`;
+  // async function fetchData() {
+  //   setLoading(true)
+  //   const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&tag=${tag}`;
 
-    const { data } = await axios.get(url);
-    const imageSource = data.data.images.downsized_large.url;
-    setGif(imageSource);
-    console.log(imageSource);
-    setLoading(false);
-  }
+  //   const { data } = await axios.get(url);
+  //   const imageSource = data.data.images.downsized_large.url;
+  //   setGif(imageSource);
+  //   console.log(imageSource);
+  //   setLoading(false);
+  // }
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+  // useEffect(() => {
+  //   fetchData();
+  // }, []);
+
+  const { gif, loading, fetchData } = useGif(tag);
 
   return (
     <div className="w-1/2 h-[450px] bg-blue-500 mx-auto rounded-lg border border-black flex flex-col items-center gap-y-5 mt-[15px]">
@@ -36,7 +38,10 @@ const Tag = () => {
         value={tag}
       />
 
-      <button className="w-10/12 bg-white text-xl py-2 rounded-lg font-bold">
+      <button
+        onClick={() => fetchData(tag)}
+        className="w-10/12 bg-white text-xl py-2 rounded-lg font-bold"
+      >
         Generate
       </button>
     </div>
